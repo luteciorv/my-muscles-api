73aa2c2cc9a4021ef79c6db6fca1aa9137bad618
Luís Felipe Rodrigues Valoura <luisfelipe.ti@b4youlog.com.br>
2025-06-30 09:18:21 -0300

    Criação da classe Notificavel e Notificacao para validar as entidades e VO no Domain.

---
+++ src/MyMuscles.Domain/Shared/Notificavel.cs
@@ -0,0 +1,20 @@
+﻿namespace MyMuscles.Domain.Shared;
+
+public abstract class Notificavel
+{
+    public IReadOnlyCollection<Notificacao> Notificacoes => _notificacoes;
+    private readonly List<Notificacao> _notificacoes = [];
+
+    public bool Valido() => _notificacoes.Count == 0;
+
+    protected void AdicionarNotificacao(string chave, string mensagem) =>
+        _notificacoes.Add(new Notificacao(chave, mensagem));
+
+    protected void AdicionarNoificacoes(List<Notificacao> notificacoes) =>
+        _notificacoes.AddRange(notificacoes);
+
+    protected virtual void Validar()
+    {
+
+    }
+}
