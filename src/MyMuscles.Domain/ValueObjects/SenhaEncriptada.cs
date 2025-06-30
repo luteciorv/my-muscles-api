1cf54eed6d307cca21853aeb10780414a93676fb
Luís Felipe Rodrigues Valoura <luisfelipe.ti@b4youlog.com.br>
2025-06-30 12:03:36 -0300

    Criação do ValueObject SenhaEncriptada.

---
+++ src/MyMuscles.Domain/ValueObjects/SenhaEncriptada.cs
@@ -0,0 +1,18 @@
+﻿using MyMuscles.Domain.Shared;
+
+namespace MyMuscles.Domain.ValueObjects;
+
+public sealed class SenhaEncriptada(string valor) : ValueObjectBase
+{
+    public string Valor { get; private set; } = valor;
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Valor;
+    }
+
+    protected override void Validar()
+    {
+        throw new NotImplementedException();
+    }
+}
