d05db45c3285e8bb7cb745a1661a5a5de56ba3a9
Luís Felipe Rodrigues Valoura <luisfelipe.ti@b4youlog.com.br>
2025-06-30 09:52:51 -0300

    Criação da classe ValueObjectBase

---
+++ src/MyMuscles.Domain/Shared/ValueObjectBase.cs
@@ -0,0 +1,29 @@
+﻿namespace MyMuscles.Domain.Shared;
+
+public abstract class ValueObjectBase : Notificavel, IEquatable<ValueObjectBase>
+{
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public bool Equals(ValueObjectBase? other)
+    {
+        if (other is null) return false;
+
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override bool Equals(object? obj) =>
+        obj is ValueObjectBase other && Equals(other);
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Aggregate(1, (current, obj) =>
+            {
+                return HashCode.Combine(current, obj != null ? obj.GetHashCode() : 0);
+            });
+    }
+
+    public static bool operator ==(ValueObjectBase a, ValueObjectBase b) => a is null && b is null || a?.Equals(b) == true;
+
+    public static bool operator !=(ValueObjectBase a, ValueObjectBase b) => !(a == b);
+}
