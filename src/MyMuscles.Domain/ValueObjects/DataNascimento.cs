01a91892146f5dbab7437369ef391280f4fc8524
Luís Felipe Rodrigues Valoura <luisfelipe.ti@b4youlog.com.br>
2025-06-30 12:25:10 -0300

    Criação do ValueObject "DataNascimento".

---
+++ src/MyMuscles.Domain/ValueObjects/DataNascimento.cs
@@ -0,0 +1,38 @@
+﻿using MyMuscles.Domain.Shared;
+
+namespace MyMuscles.Domain.ValueObjects;
+
+public sealed class DataNascimento : ValueObjectBase
+{
+    public DataNascimento(DateTime valor)
+    {
+        Valor = valor;
+        Validar();
+    }
+
+    public DateTime Valor { get; private set; }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Valor;
+    }
+
+    protected override void Validar()
+    {
+        var hoje = DateTime.Today;
+        var limiteMin = hoje.AddYears(-120);
+        var limiteMax = hoje.AddYears(-16);
+
+        if (Valor > hoje)
+        {
+            AdicionarNotificacao(nameof(DataNascimento), "O campo 'data de nascimento' não pode ser no futuro.");
+            return;
+        }
+
+        if (Valor < limiteMin)
+            AdicionarNotificacao(nameof(DataNascimento), "O campo 'data de nascimento' não pode indicar mais de 120 anos.");
+
+        if (Valor > limiteMax)
+            AdicionarNotificacao(nameof(DataNascimento), "O campo 'data de nascimento' deve ter no mínimo 16 anos.");
+    }
+}
