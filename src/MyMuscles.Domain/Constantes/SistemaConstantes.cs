2ad9138a44b6200facbd5bd29fab280e2de9b5c4
Luís Felipe Rodrigues Valoura <luisfelipe.ti@b4youlog.com.br>
2025-07-01 11:02:17 -0300

    Criação do Value Object "IMC"

--- src/MyMuscles.Domain/Constantes/SistemaConstantes.cs
+++ src/MyMuscles.Domain/Constantes/SistemaConstantes.cs
@@ -5,4 +5,10 @@ public static class SistemaConstantes
     public const int IdadeMinima = 16;
     public const int IdadeMaxima = 120;
     public const decimal AlturaMaxima = 3.00m;
+
+    public const decimal LimiteAbaixoDoPeso = 18.5m;
+    public const decimal LimitePesoNormal = 25.0m;
+    public const decimal LimiteSobrepeso = 30.0m;
+    public const decimal LimiteObesidadeGrau1 = 35.0m;
+    public const decimal LimiteObesidadeGrau2 = 40.0m;
 }
