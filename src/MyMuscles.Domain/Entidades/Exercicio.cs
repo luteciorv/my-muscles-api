64e4b21e372571b94cdcc7c495a1f4e2288d4695
Luís Felipe Rodrigues Valoura <luisfelipe.ti@b4youlog.com.br>
2025-07-01 16:01:51 -0300

    Criação da entidade "Exercicio".

---
+++ src/MyMuscles.Domain/Entidades/Exercicio.cs
@@ -0,0 +1,58 @@
+﻿using MyMuscles.Domain.Shared;
+using MyMuscles.Domain.ValueObjects;
+using MyMuscles.Domain.ValueObjects.Informacoes;
+
+namespace MyMuscles.Domain.Entidades;
+
+public sealed class Exercicio : EntidadeBase
+{
+    public Exercicio(Nome nome, Descricao descricao, Repeticao series, Repeticao repeticao, List<Foto> fotos)
+    {
+        Nome = nome;
+        Descricao = descricao;
+        Series = series;
+        Repeticao = repeticao;
+        _fotos = fotos;
+        Concluido = false;
+        Validar();
+    }
+
+    public Nome Nome { get; private set; }
+    public Descricao Descricao { get; private set; }
+    public Repeticao Series { get; private set; }
+    public Repeticao Repeticao { get; private set; }
+    public bool Concluido { get; private set; }
+
+    public IReadOnlyCollection<Foto> Fotos => _fotos;
+    private readonly List<Foto> _fotos;
+
+    public void Concluir()
+    {
+        if (Concluido)
+        {
+            AdicionarNotificacao(nameof(Exercicio), "Exercício já concluído.");
+            return;
+        }
+
+        AtualizadoEm = DateTime.UtcNow;
+        Concluido = true;
+    }
+
+    protected override void Validar()
+    {
+        if (!Nome.Valido)
+            AdicionarNotificacoes([.. Nome.Notificacoes]);
+
+        if (!Series.Valido)
+            AdicionarNotificacoes([.. Series.Notificacoes]);
+
+        if (!Repeticao.Valido)
+            AdicionarNotificacoes([.. Repeticao.Notificacoes]);
+
+        if (Fotos.Any(f => !f.Valido))
+        {
+            List<Notificacao> erros = [.. Fotos.Where(f => !f.Valido).SelectMany(f => f.Notificacoes)];
+            AdicionarNotificacoes(erros);
+        }
+    }
+}
---
+++ src/MyMuscles.Domain/Extensions/CollectionExtension.cs
@@ -0,0 +1,6 @@
+﻿namespace MyMuscles.Domain.Extensions;
+
+public static class CollectionExtension
+{
+    public static bool Vazio<T>(this IEnumerable<T>? collection) => collection is null || !collection.Any();
+}
