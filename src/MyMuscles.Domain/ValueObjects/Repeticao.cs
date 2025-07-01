bec84179bb68572bda4edc898e790eada7cbfad1
Luís Felipe Rodrigues Valoura <luisfelipe.ti@b4youlog.com.br>
2025-07-01 12:08:00 -0300

    Criação do Value Object "Repeticao".

---
+++ src/MyMuscles.Domain/ValueObjects/Repeticao.cs
@@ -0,0 +1,26 @@
+﻿using MyMuscles.Domain.Mensagens;
+using MyMuscles.Domain.Shared;
+
+namespace MyMuscles.Domain.ValueObjects;
+
+public sealed class Repeticao : ValueObjectBase
+{
+    public Repeticao(int quantidade)
+    {
+        Quantidade = quantidade;
+        Validar();
+    }
+
+    public int Quantidade { get; private set; }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Quantidade;
+    }
+
+    protected override void Validar()
+    {
+        if(Quantidade <= 0)
+            AdicionarNotificacao(nameof(Repeticao), MensagensExtension.ApenasValorPositivo(nameof(Repeticao)));
+    }
+}
